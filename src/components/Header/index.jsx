fa2e92b934d40046255f09212b9dcb656a7c2fdd
Michael Aubry <michealaubrydesign@gmail.com>
2023-07-26 15:08:20 +0200

    Composant header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,31 @@
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/logo.png'
-import '../../sass/main.scss'
 
 function Header() {
+  const location = useLocation()
   return (
     <nav className="nav">
       <img src={logo} alt="Kasa" className="kasa-logo" />
       <div className="nav__links">
-        <Link to="/" className="nav__links__link">
+        <Link
+          to="/"
+          className={
+            location.pathname === '/'
+              ? 'nav__links__link active'
+              : 'nav__links__link'
+          }
+        >
           Accueil
         </Link>
-        <Link to="/apropos" className="nav__links__link">
+        <Link
+          to="/apropos"
+          className={
+            location.pathname === '/apropos'
+              ? 'nav__links__link active'
+              : 'nav__links__link'
+          }
+        >
           A propos
         </Link>
       </div>
