e03d3461c5adf89b2372205375c4ead3f800e0cf
Michael Aubry <michealaubrydesign@gmail.com>
2023-07-26 17:16:46 +0200

    Ajout composant collapse

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function Collapse({ title, content }) {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleCollapse = () => {
+    setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+  }
+
+  return (
+    <div className={`collapse ${isExpanded ? 'expanded' : 'collapsed'}`}>
+      <div className="collapse__header">
+        <h3>{title}</h3>
+        <div className="chevron-container" onClick={toggleCollapse}>
+          <i
+            className={`fa-solid fa-chevron-${isExpanded ? 'up' : 'down'}`}
+          ></i>
+        </div>
+      </div>
+      {isExpanded && <div className="collapse__content">{content}</div>}
+    </div>
+  )
+}
+
+export default Collapse
