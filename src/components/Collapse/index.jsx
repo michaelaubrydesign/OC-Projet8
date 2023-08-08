f1b8b5513ebdea9ced0ac148e14f932d5c6a0bfb
Michael Aubry <michealaubrydesign@gmail.com>
2023-08-08 12:23:59 +0200

    Ajout commentaires

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 
 function Collapse({ title, content }) {
+  // Utilisation du hook useState pour gérer l'état d'expansion du contenu
   const [isExpanded, setIsExpanded] = useState(false)
 
+  // Fonction appelée lorsqu'on clique sur le bouton d'expansion/réduction.
   const toggleCollapse = () => {
+    // Utilisation de la fonction de mise à jour du useState avec une fonction pour maintenir l'état précédent.
     setIsExpanded((prevIsExpanded) => !prevIsExpanded)
   }
 
